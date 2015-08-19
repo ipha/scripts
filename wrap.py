0287a779b5f0e539142b79e1150f9a336935d31e
Mitch Bigelow <ipha00@gmail.com>
2015-08-19 09:13:49 -0400

    wrap text files

---
+++ wrap.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+
+width = 80
+last_newline = 0
+
+if len(sys.argv) >= 2:
+    try:
+        width = int(sys.argv[1])
+    except ValueError:
+        print("Invalid line width")
+
+for line in sys.stdin:
+    sline = line.rstrip()
+    if sline == "":
+        last_newline = last_newline + 1
+    else:
+        if last_newline > 1:
+            print("\n" * (last_newline - 1), end="")
+        last_newline = 0
+
+        words = sline.split(' ');
+        chars = 0
+        for word in words:
+            if chars + len(word) + 1 > width:
+                print("\n", end="")
+                chars = 0
+            print(word + " ", end="")
+            chars = len(word) + 1 + chars
+        print("\n", end="")
+
