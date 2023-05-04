7717013e3812fe776684651fdff4f44b4891fc83
Mitch Bigelow <ipha00@gmail.com>
2023-05-04 10:01:16 -0400

    systemd: don't override system units

--- services/deploy.sh
+++ services/deploy.sh
@@ -2,4 +2,8 @@
 
 sudo cp ./*.service ./*.timer /etc/systemd/system/
 sudo cp ./user/*.service /etc/systemd/user/
+
+[ -f /usr/lib/systemd/system/certbot-renew.service ] && sudo rm /etc/systemd/system/certbot-renew.service
+[ -f /usr/lib/systemd/system/certbot-renew.timer ] && sudo rm /etc/systemd/system/certbot-renew.timer
+
 sudo systemctl daemon-reload
