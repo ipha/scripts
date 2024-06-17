24d0d0fa8231e0957dc0a7492a7831864cfe596b
Mitch Bigelow <ipha00@gmail.com>
2024-06-17 10:31:17 -0400

    zsh: update backup prompt

--- config/ps1.zsh
+++ config/ps1.zsh
@@ -1,24 +1,29 @@
-# LOCK=\xEE\x82\xA2
-# SEP=\xEE\x82\xB0
-# SPACE=\xC2\xA0
+C1=90
+C2=168
+C3=216
+C4=110
+C5=30
+C6=24
 
-F1=220
-B1=166
-F2=231
-B2=31
-F3=252
-B3=240
-
+DISPUSER=""
+DISPHOST=""
 
 if [ "$USER" = 'root' ]; then
-    B2=160
+    C1=196
+    DISPUSER="%n"
 fi
 
 if [ -n "$SSH_CLIENT" ]; then
-    PS1_1="%F{$F1}%K{$B1} %m %F{$B1}%K{$B2}"
+    DISPUSER="%n"
+    DISPHOST="@%M"
 fi
 
 PS1_2="%F{$F2}%K{$B2} %n %F{$B2}%K{$B3}"
 PS1_3="%F{$F3}%K{$B3} %~ %F{$B3}%k %f"
+SEG0="%F{$C1}%f"
+SEG1="%K{$C1}$DISPUSER$DISPHOST%k%F{$C1}%K{$C2}%f%k"
+SEG2="%K{$C2} %~ %F{$C2}%K{$C3}%f%k"
+SEG3="%K{$C3}%F{$C3}%K{$C4}%f%k"
+SEG4="%K{$C4}%F{$C4}%k%f%k"
 
-PROMPT="${PS1_1}${PS1_2}${PS1_3}"
+PROMPT="${SEG0}${SEG1}${SEG2}${SEG3}${SEG4}"
